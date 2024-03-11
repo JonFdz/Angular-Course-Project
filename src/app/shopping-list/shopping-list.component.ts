85c9a2ee343235222041e7c012794c371d0f7919
Jon Fernandez <jfernandez@nechigroup.com>
2024-03-11 13:13:52 +0100

    Creating an "ingredient" Model

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
-  selector: 'app-shopping-list',
-  templateUrl: './shopping-list.component.html',
-  styleUrl: './shopping-list.component.scss'
+	selector: 'app-shopping-list',
+	templateUrl: './shopping-list.component.html',
+	styleUrl: './shopping-list.component.scss'
 })
-export class ShoppingListComponent {
+export class ShoppingListComponent implements OnInit {
+	ingredients: Ingredient[] = [
+		new Ingredient('Apples', 5),
+		new Ingredient('Tomatoes', 10),
+	];
+
+	constructor() { }
+
+	ngOnInit() {
+	}
+
+	onIngredientAdded(ingredient: Ingredient) {
+		this.ingredients.push(ingredient);
+	}
 
 }
