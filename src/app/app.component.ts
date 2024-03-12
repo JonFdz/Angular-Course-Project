d9d3bfc2a30b88877dfa76473d6ce7f5f56f67de
JonFdz <jonfernal@gmail.com>
2024-03-12 09:22:40 +0100

    Adding navigation with event binding and ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'course-project-app';
+	title = 'course-project-app';
+	recipesShow = true;
+	shoppingListShow = false;
+
+	onRecipesShow(event: boolean) {
+		console.log('Recipes clicked');
+		this.recipesShow = event;
+		this.shoppingListShow = !event;
+	}
+
+	onShoppingListShow(event) {
+		console.log('Shopping List clicked');
+		this.shoppingListShow = event;
+		this.recipesShow = !event;
+	}
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+	selector: 'app-header',
+	templateUrl: './header.component.html',
+	styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
+	@Output() onRecipesClicked: EventEmitter<boolean> = new EventEmitter<boolean>();
+	@Output() onShoppingListClicked: EventEmitter<boolean> = new EventEmitter<boolean>();
 	collapsed = true;
 
+	onClickRecipes() {
+		console.log('Recipes clicked');
+		this.onRecipesClicked.emit(true);
+	}
+
+	onClickShoppingList() {
+		console.log('Shopping List clicked');
+		this.onShoppingListClicked.emit(true);
+	}
 }
